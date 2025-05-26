d656ebb990adb4558f646d8c199e927b5eda68c1
Ryan Roden-Corrent <git@rrc.codes>
2025-05-26 15:22:20 -0400

    Use ANSI theme.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use ansi_to_tui::IntoText as _;
 use anyhow::Result;
 use clap::Parser;
 use crossterm::event::{Event, EventStream, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
@@ -11,17 +10,23 @@ use nucleo::{
 use ratatui::{
     layout::{Constraint, Direction, Layout, Position},
     style::{Style, Stylize},
+    text::{Line, Span, Text},
     widgets::{Block, Borders, Paragraph, Row, Table, TableState},
     DefaultTerminal, Frame,
 };
 use std::collections::HashMap;
-use std::{collections::hash_map::Entry, sync::Arc};
+use std::{collections::hash_map::Entry, io::Cursor, sync::Arc};
 use syntect::{
-    easy::HighlightLines, highlighting::ThemeSet, parsing::SyntaxSet, util::LinesWithEndings,
+    easy::HighlightLines,
+    highlighting::{self, Theme, ThemeSet},
+    parsing::SyntaxSet,
+    util::LinesWithEndings,
 };
 use tokio::sync::mpsc::{self, Receiver};
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _};
+
+const ANSI_THEME: &[u8] = include_bytes!("ansi.tmTheme");
 
 fn get_auth_token() -> Result<String> {
     let mut cmd = std::process::Command::new("gh");
@@ -43,14 +48,14 @@ pub struct App {
     exit: bool,
     table_state: TableState,
     content_client: ContentClient,
-    content_cache: HashMap<String, String>, // url->content
+    content_cache: HashMap<String, Text<'static>>, // url->content
     nucleo: Nucleo<SearchItem>,
     nucleo_rx: Receiver<()>,
     pattern: String,
     cursor_pos: usize,
 
     syntax: SyntaxSet,
-    themes: ThemeSet,
+    theme: Theme,
 }
 
 impl App {
@@ -74,6 +79,8 @@ impl App {
                 });
             }),
         );
+
+        let mut theme_cursor = Cursor::new(ANSI_THEME);
         Self {
             event_stream: EventStream::default(),
             exit: false,
@@ -85,7 +92,7 @@ impl App {
             pattern: String::new(),
             cursor_pos: 0,
             syntax: SyntaxSet::load_defaults_newlines(),
-            themes: ThemeSet::load_defaults(),
+            theme: ThemeSet::load_from_reader(&mut theme_cursor).expect("Loading theme"),
         }
     }
 
@@ -160,13 +167,12 @@ impl App {
             }
         };
 
-        let content = snap
+        let text = snap
             .get_matched_item(idx.try_into().unwrap())
             .and_then(|item| self.content_cache.get(&item.data.url))
-            .map(|s| s.as_str())
-            .unwrap_or("");
+            .cloned()
+            .unwrap_or_default();
 
-        let text = content.into_text().unwrap();
         let preview = Paragraph::new(text).block(Block::bordered());
         frame.render_widget(preview, preview_area);
     }
@@ -205,17 +211,89 @@ impl App {
         Ok(())
     }
 
+    fn to_ansi_color(color: highlighting::Color) -> Option<ratatui::style::Color> {
+        if color.a == 0 {
+            // Themes can specify one of the user-configurable terminal colors by
+            // encoding them as #RRGGBBAA with AA set to 00 (transparent) and RR set
+            // to the 8-bit color palette number. The built-in themes ansi, base16,
+            // and base16-256 use this.
+            Some(match color.r {
+                // For the first 8 colors, use the Color enum to produce ANSI escape
+                // sequences using codes 30-37 (foreground) and 40-47 (background).
+                // For example, red foreground is \x1b[31m. This works on terminals
+                // without 256-color support.
+                0x00 => ratatui::style::Color::Black,
+                0x01 => ratatui::style::Color::Red,
+                0x02 => ratatui::style::Color::Green,
+                0x03 => ratatui::style::Color::Yellow,
+                0x04 => ratatui::style::Color::Blue,
+                0x05 => ratatui::style::Color::Magenta,
+                0x06 => ratatui::style::Color::Cyan,
+                0x07 => ratatui::style::Color::White,
+                // For all other colors, use Fixed to produce escape sequences using
+                // codes 38;5 (foreground) and 48;5 (background). For example,
+                // bright red foreground is \x1b[38;5;9m. This only works on
+                // terminals with 256-color support.
+                //
+                // TODO: When ansi_term adds support for bright variants using codes
+                // 90-97 (foreground) and 100-107 (background), we should use those
+                // for values 0x08 to 0x0f and only use Fixed for 0x10 to 0xff.
+                n => ratatui::style::Color::Indexed(n),
+            })
+        } else if color.a == 1 {
+            // Themes can specify the terminal's default foreground/background color
+            // (i.e. no escape sequence) using the encoding #RRGGBBAA with AA set to
+            // 01. The built-in theme ansi uses this.
+            None
+        } else {
+            Some(ratatui::style::Color::Rgb(color.r, color.g, color.b))
+        }
+    }
+
+    // Convert syntect highlighting to ANSI terminal colors
+    // See https://github.com/trishume/syntect/issues/309
+    // Borrowed from https://github.com/sxyazi/yazi/pull/460/files
+    fn to_line_widget(regions: Vec<(highlighting::Style, &str)>) -> Line<'static> {
+        let mut line = Line::default();
+        for (style, s) in regions {
+            let mut modifier = ratatui::style::Modifier::empty();
+            if style.font_style.contains(highlighting::FontStyle::BOLD) {
+                modifier |= ratatui::style::Modifier::BOLD;
+            }
+            if style.font_style.contains(highlighting::FontStyle::ITALIC) {
+                modifier |= ratatui::style::Modifier::ITALIC;
+            }
+            if style
+                .font_style
+                .contains(highlighting::FontStyle::UNDERLINE)
+            {
+                modifier |= ratatui::style::Modifier::UNDERLINED;
+            }
+
+            line.push_span(Span {
+                content: s.to_string().into(),
+                style: ratatui::style::Style {
+                    fg: Self::to_ansi_color(style.foreground),
+                    // bg: Self::to_ansi_color(style.background),
+                    add_modifier: modifier,
+                    ..Default::default()
+                },
+            })
+        }
+
+        line
+    }
+
     fn process_content(&mut self, url: String, content: String) {
         tracing::debug!("Caching content for: {url}");
         let syntax = self.syntax.find_syntax_by_extension("rs").unwrap();
-        let mut h = HighlightLines::new(syntax, &self.themes.themes["Solarized (dark)"]);
-        let text = LinesWithEndings::from(&content)
-            .map(|line| {
-                let ranges = h.highlight_line(line, &self.syntax).unwrap();
-                syntect::util::as_24_bit_terminal_escaped(&ranges[..], true)
-            })
-            .collect::<Vec<_>>()
-            .join("");
+        let mut h = HighlightLines::new(syntax, &self.theme);
+
+        let mut text = Text::default();
+        for line in LinesWithEndings::from(&content) {
+            let ranges = h.highlight_line(line, &self.syntax).unwrap();
+            text.push_line(Self::to_line_widget(ranges))
+        }
         self.content_cache.insert(url, text);
     }
 
