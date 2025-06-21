a79e160d7feaa6986bf587cd26003c9f5cac9b98
Ryan Roden-Corrent <git@rrc.codes>
2025-06-21 17:15:08 -0400

    Separate code TUI

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,29 @@
+use clap::{Args, Parser, Subcommand};
+
+pub mod code;
 pub mod github;
 pub mod preview;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    /// Search code.
+    Code(QueryArgs),
+}
+
+#[derive(Args, Default)]
+pub struct QueryArgs {
+    /// Query to search.
+    pub query: String,
+
+    /// Maximum number of result pages.
+    #[arg(short, long, default_value_t = 5)]
+    pub pages: usize,
+}
