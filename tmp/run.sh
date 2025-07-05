71a6da7ca515950cb61d978b33802b5a1a0e5909
Ryan Roden-Corrent <git@rrc.codes>
2025-07-04 20:47:02 -0400

    Update run script

--- tmp/run.sh
+++ tmp/run.sh
@@ -1,5 +1,7 @@
 args="$(cat $1)"
 
 jq '{"query": $query, "variables": .}' --rawfile query $1 $2 \
-  | curl -s -H 'Accept: application/vnd.github.text-match+json' -H "Authorization: bearer $(gh auth token)" -X POST -d @- https://api.github.com/graphql \
+  | curl -v -s \
+    -H "Authorization: bearer $(gh auth token)" \
+    -X POST -d @- https://api.github.com/graphql \
   | jq .
